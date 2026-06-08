e597361838a2160c8ff269ac935f0c0762b29b59
Yeshwant.g.n <25f3003706@ds.study.iitm.ac.in>
2026-06-08 06:15:33 +0000

    day1

---
+++ week1/logic_gates/rtl/logic_gates.v
@@ -0,0 +1,34 @@
+module and_gate(
+    input a,
+    input b,
+    output y
+);
+assign y = a & b;
+endmodule
+
+
+module or_gate(
+    input a,
+    input b,
+    output y
+);
+assign y = a | b;
+endmodule
+
+
+module xor_gate(
+    input a,
+    input b,
+    output y
+);
+
+assign y = a ^ b;
+endmodule
+
+
+module not_gate(
+    input a,
+    output y
+);
+assign y = ~a;
+endmodule
